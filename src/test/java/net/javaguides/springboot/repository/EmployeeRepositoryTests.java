14bcfe43ff3a092d5a2e0a47f424ac5ee83f755d
Miguel Diaz <71565450+libypur2k20@users.noreply.github.com>
2022-02-26 20:38:10 +0100

    Initial Commit

---
+++ src/test/java/net/javaguides/springboot/repository/EmployeeRepositoryTests.java
@@ -0,0 +1,36 @@
+package net.javaguides.springboot.repository;
+
+import net.javaguides.springboot.model.Employee;
+//import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class EmployeeRepositoryTests {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //JUnit test for save employee operation.
+    @DisplayName("JUnit test for save employee operation")
+    @Test
+    public void givenEmployeeObject_whenSave_thenReturnSavedEmployee(){
+
+        //given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .email("ramesh@gmail.com")
+                .build();
+
+        //when - action or the behaviour that we are going test
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        //then - verify the output
+        assertThat(savedEmployee).isNotNull();
+        assertThat(savedEmployee.getId()).isGreaterThan(0);
+    }
+}
